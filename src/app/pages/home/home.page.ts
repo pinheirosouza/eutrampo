b96277a863d8bd7b5b1efa5cddb6c761db6cb162
Pedro <pinheiro@furg.br>
2020-01-13 19:02:24 -0300

    widget de contratação

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -8,8 +8,33 @@ import { MenuController } from '@ionic/angular';
 })
 export class HomePage {
 
+  public recent_services;
+
   constructor(
     public menuCtrl: MenuController
-  ) {}  
+  ) {
+    this.recent_services = [
+      { name:'Pedro Souza', 
+        pic:"../../../assets/img/pedro.jpg", 
+        servicetype:'Pedreiro', 
+        provided: 145, 
+        price: 45, 
+        redirectTo: "#"},
+
+      { name:'Fernando Toledo', 
+        pic:"../../../assets/img/fernando.jpeg", 
+        servicetype:'Costureiro', 
+        provided: 184, 
+        price: 20, 
+        redirectTo: "#"},
+
+      { name:'Felipe Freitas', 
+        pic:"../../../assets/img/felipe.png", 
+        servicetype:'Professor Particular', 
+        provided: 82, 
+        price: 60, 
+        redirectTo: "#"},
+    ]
+  }  
 
 }
