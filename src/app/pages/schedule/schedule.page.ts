077082fbd52a13af8f03358d782413ae4d0884ee
Amarakk <arthuramaral.souza@gmail.com>
2020-01-18 20:25:53 -0300

    Agenda ok

--- src/app/pages/schedule/schedule.page.ts
+++ src/app/pages/schedule/schedule.page.ts
@@ -1,15 +1,129 @@
-import { Component, OnInit } from '@angular/core';
+import { ScheduleService } from './schedule.service';
+import { Component, ViewEncapsulation, ViewChild, OnInit, Inject, LOCALE_ID } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { formatDate } from '@angular/common';
+import { CalendarComponent } from 'ionic2-calendar/calendar';
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.page.html',
   styleUrls: ['./schedule.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SchedulePage implements OnInit {
 
-  constructor() { }
-
+  event = {
+    title: '',
+    startTime: '',
+    endTime: '',
+    allDay: false
+  };
+  mesAtual = {data:''};
+ 
+  minDate = new Date().toISOString();
+ 
+  eventSource = [];
+  viewTitle;
+ 
+  calendar = {
+    mode: 'month',
+    currentDate: new Date(),
+  };
+ 
+  @ViewChild(CalendarComponent,{static:false}) myCal: CalendarComponent;
+ 
+  constructor(private alertCtrl: AlertController, private scheduleService: ScheduleService, @Inject(LOCALE_ID) private locale: string) { }
+ 
   ngOnInit() {
+    this.resetEvent();
+  }
+  
+ 
+  resetEvent() {
+    this.event = {
+      title: '',
+      startTime: new Date().toISOString(),
+      endTime: new Date().toISOString(),
+      allDay: false
+    };
+  }
+ 
+  // Create the right event format and reload source
+  addNewEvent() {
+    let eventCopy = {
+      title: this.event.title,
+      startTime:  new Date(this.event.startTime),
+      endTime: new Date(this.event.endTime),
+      allDay: this.event.allDay
+    }
+ 
+    if (eventCopy.allDay) {
+      let start = eventCopy.startTime;
+      let end = eventCopy.endTime;
+ 
+      eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()));
+      eventCopy.endTime = new Date(Date.UTC(end.getUTCFullYear(), end.getUTCMonth(), end.getUTCDate() + 1));
+    }
+ 
+    this.eventSource.push(eventCopy);
+    this.myCal.loadEvents();
+    this.resetEvent();
+    console.log(this.eventSource)
+    this.scheduleService.setTarefas(this.eventSource);
+  }
+   // Change current month/week/day
+ next() {
+  var swiper = document.querySelector('.swiper-container')['swiper'];
+  swiper.slideNext();
+}
+ 
+back() {
+  var swiper = document.querySelector('.swiper-container')['swiper'];
+  swiper.slidePrev();
+}
+ 
+// Change between month/week/day
+changeMode(mode) {
+  this.calendar.mode = mode;
+}
+ 
+// Focus today
+today() {
+  this.calendar.currentDate = new Date();
+}
+ 
+// Selected date range and hence title changed
+onViewTitleChanged(title) {
+  this.viewTitle = title;
+  title = title.charAt(0).toUpperCase()+ title.slice(1)
+  var pos = title.search(",");
+  if(pos>0){
+    title = title.substring(0,pos)
   }
 
+  this.mesAtual.data=title
+
+}
+ 
+// Calendar event was clicked
+async onEventSelected(event) {
+  // Use Angular date pipe for conversion
+  let start = formatDate(event.startTime, 'medium', this.locale);
+  let end = formatDate(event.endTime, 'medium', this.locale);
+ 
+  const alert = await this.alertCtrl.create({
+    header: event.title,
+    message: 'De: ' + start + '<br><br>Até: ' + end,
+    buttons: ['OK']
+  });
+  alert.present();
+}
+ 
+// Time slot was clicked
+onTimeSelected(ev) {
+  let selected = new Date(ev.selectedTime);
+  this.event.startTime = selected.toISOString();
+  selected.setHours(selected.getHours() + 1);
+  this.event.endTime = (selected.toISOString());
+}
 }
