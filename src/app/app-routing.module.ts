8eb5fcd3e6554ab93371cc485e696cd99fba4d38
Pedro Pinheiro <pinheiro@furg.br>
2020-03-14 18:48:35 -0300

    stage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -82,8 +82,6 @@ const routes: Routes = [
   {
     path: 'add-service-modal',
     loadChildren: () => import('./shared/modals/add-service-modal/add-service-modal.module').then( m => m.AddServiceModalPageModule)
-  }
-
   },
   {
     path: '',
